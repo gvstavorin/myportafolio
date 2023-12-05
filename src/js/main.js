650899feb159acff57bc818a28ccdce49a98ff6f
gvstavorin <gustavoandreescg@gmail.com>
2023-12-04 22:15:40 -0300

    Correcion de menu navbar buton

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 const nav = document.querySelector("#navbar");
+
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
@@ -9,7 +10,21 @@ abrir.addEventListener("click",()=>{
     nav.classList.add("visible");
 })
 
+abrir.addEventListener('touchstart', ()=>{
+
+    nav.classList.add("visible");
+});
+
+
+
+
 cerrar.addEventListener("click",()=>{
+
+    nav.classList.remove("visible");
+})
+
+
+cerrar.addEventListener("touchstart",()=>{
 
     nav.classList.remove("visible");
 })
