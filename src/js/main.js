90bb5193578b28bc33b9a519342c719956ba74ff
gvstavorin <gustavoandreescg@gmail.com>
2023-12-04 22:33:14 -0300

    Se removio touchstart en js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 const nav = document.querySelector("#navbar");
-
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
@@ -10,21 +9,7 @@ abrir.addEventListener("click",()=>{
     nav.classList.add("visible");
 })
 
-abrir.addEventListener('touchstart', ()=>{
-
-    nav.classList.add("visible");
-});
-
-
-
-
 cerrar.addEventListener("click",()=>{
-
-    nav.classList.remove("visible");
-})
-
-
-cerrar.addEventListener("touchstart",()=>{
 
     nav.classList.remove("visible");
 })
